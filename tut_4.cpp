e6650a8928b67799e6cf03c6ac55ed88a2900477
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-02 22:41:53 +0530

    Practices

---
+++ tut_4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int glo=6;
+void sum(){
+    int a;
+}
+
+int main(){
+    int glo=9;
+    int a=10, b=15;
+    float pi=3.15;
+    char c='p';
+    sum();
+    cout<<glo;
+
+    
+
+    return 0;
+}
