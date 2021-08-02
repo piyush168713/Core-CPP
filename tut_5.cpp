237c13d02ea5af6a26903add147d442c872c631b
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-02 22:42:47 +0530

    cpp basics

---
+++ tut_5.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<<"Enter the value of num1: ";
+    cin>>num1;
+    cout<<"Enter the value of num2: ";
+    cin>>num2;
+    cout<<"sum is: "<<num1+num2;
+    return 0;
+}
