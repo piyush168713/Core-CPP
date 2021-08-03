3c3afc0320c93861f6a35099f42f6c50a62107db
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-03 14:30:58 +0530

    basic for if else and switch case

---
+++ tut_9.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    
+    int age;
+    cout<<"Tell me your age: ";
+    cin>>age;   
+
+
+// /******** Selection control structure: If else - If else ladder **********/
+
+    // if((age<18) && (age>0)){
+    //     cout<<"You can not come to party\n";
+    // }
+    // else if(age==18){
+    //     cout<<"You are a kid and you will get a kid pass for the party\n";
+    // }
+    // else if(age<1){
+    //     cout<<"Invalid age";
+    // }
+    // else if (age<50){
+    //     cout<<"You can come to party\n";
+    // }   
+    // else{
+    //     cout<<"You are overaged";
+    // }
+
+// /******** Selection control structure: Switch case statements **********/
+
+    switch (age)
+    {
+    case 18:
+        cout<<"You are 18\n";
+        break;
+    case 22:
+        cout<<"You are 22\n";
+        break;
+    case 3:
+        cout<<"You are 3\n";
+        break;
+
+    default:
+    cout<<"No special cases";
+        break;
+    }
+    cout<<"Done with switch case";
+
+    return 0;
+}
