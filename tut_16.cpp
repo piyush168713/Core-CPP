ccf8636aff2fd790f03850f681a8d882c0a062ac
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-07 14:50:34 +0530

    Call by Value & Call by Reference in C++

---
+++ tut_16.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int sum(int a, int b){
+    int c= a+b;
+    return c;
+}
+
+// void swap(int a, int b){  // temp  a  b    //----> This will not work <----- //
+//     int temp = a;         // 4     4  5
+//     a = b;                // 4     5  5 
+//     b = temp;             // 4     5  4
+// }
+
+void swapPointer(int* a, int* b){  // temp  a  b    
+    int temp = *a;         // 4     4  5
+    *a = *b;                // 4     5  5 
+    *b = temp;             // 4     5  4
+}
+
+
+int main(){
+    int x=5, y=6;
+    // cout<<"The sum of 4 and 5 is "<<sum(a, b);
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
+
+    // swap(x,y);     //----> This will not work <----- //
+    swapPointer(&x, &y);
+
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
+    
+    return 0;
+} 
