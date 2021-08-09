23be31b6588bf014f25acde031b05ad23f666289
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-09 23:44:50 +0530

    Classes, Public and Private access modifiers in C++

--- tut_21.cpp
+++ tut_21.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 // /********// /********Function Overloading with Examples in C++ **********/ **********/
 
-class employee
+class employee   // The class
 {
-    private:
+    private:     // Access specifier
     int a, b, c;
 
-    public:
+    public:      // Access specifier
     int d, e;
-    void setData(int a, int b, int c);  // Declaration mtlb apko aage mil jayega
+    void setData(int a, int b, int c);  // Declaration mtlb apko aage mil jayega   // Method/function defined inside the class
     void getData(){
         cout<<"The value of a is "<<a<<endl;
         cout<<"The value of b is "<<b<<endl;
@@ -20,17 +20,17 @@ class employee
     }
 };
 
-void employee :: setData(int a1, int b1, int c1){
+void employee :: setData(int a1, int b1, int c1){      // Method/function definition outside the class
     a = a1;
     b = b1;
     c = c1;
 }
 
 int main(){
-    employee piyush;
+    employee piyush;    // Create an object of MyClass
     // piyush.a = 10;   ----> This will throw error as a is private.
     piyush.d = 16;
-    piyush.e = 87;
+    piyush.e = 87;     // Call the method
     piyush.setData(1,2,3);
     piyush.getData();
     return 0;
