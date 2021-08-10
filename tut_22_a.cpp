b72e3645273bfc30b3dd2e357f464c656199156d
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-10 14:57:53 +0530

    OOPs Recap & Nesting of Member Functions in C++

---
+++ tut_22_a.cpp
@@ -0,0 +1,89 @@
+
+    // oops -- classes and objects
+    // class --> extensions of structure (in C)
+
+    // structures and Limitations
+    // ---> members are public
+    // ---> No methods
+    // classes ---> structures + more
+    // classes ---> can have methods and properties
+    // classes ---> can have few mwmbers as private & few as public
+    // structures in C++ are typedefed
+    // you can declare object along with the class declaration like this:
+    /* class employee{
+        // class definition
+    } piyush, kumar, rohan; */
+
+    // piyush.salary = 8 makes no sense if salary is private
+
+    // Nesting of member functions
+    
+#include <iostream>
+#include <string>
+using namespace std;
+
+class binary
+{
+private:
+    string s;
+    void chk_bin(void);                          // ---> who are you?  means identity
+
+public:
+    void read(void);                             // ---> who are you?  means identity
+
+    void ones_compliment(void);                  // ---> who are you?  means identity
+    void display(void);                          // ---> who are you?  means identity
+ 
+};
+
+void binary :: read(void){                      // ---> what's ur work,,,  
+    cout<<"Enter a binary number"<<endl;
+    cin>>s;
+}
+
+void binary :: chk_bin(void){                   // ---> what's ur work,,,  
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i)!='0' && s.at(i)!='1')
+        {
+            cout<<"Incorrect binary format"<<endl;
+            exit(0);
+        }     
+    }
+    
+}
+
+void binary :: ones_compliment(void)             // ---> what's ur work,,,  
+{
+    chk_bin();                           // example of nested class
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s.at(i) == '0'){
+            s.at(i) = '1';
+        }
+        else{
+            s.at(i) = '0';
+        }
+    }
+
+}
+
+void binary :: display(void){                   // ---> what's ur work,,,  
+    cout<<"\nDisplaying your Binary number"<<endl;
+    for (int i = 0; i < s.length(); i++){
+        cout<<s.at(i);
+    }
+
+}
+
+int main()
+{
+    binary b;
+    b.read();                                          // ---> show me your work 
+//  b.chk_bin();   ----> This will throw error as chk_bin is private.
+    b.display();                                       // ---> show me your work 
+    b.ones_compliment();                               // ---> show me your work 
+    b.display();                                       // ---> show me your work 
+
+    return 0;
+}
