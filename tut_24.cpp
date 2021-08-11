bd79eb052e66ec71fe05cc9de0e8b7414bd53a67
Piyush Kumar <81097447+piyush168713@users.noreply.github.com>
2021-08-11 13:54:33 +0530

    Static Data Members & Methods in C++ OOPS

--- tut_24.cpp
+++ tut_24.cpp
@@ -18,6 +18,10 @@ public:
     {
         cout << "The id of this employee is " << id << " and this is employee number " << count << endl;
     }
+    static void getCount(void){
+        // cout<<id;    // throws an error
+        cout<<"The value of count is "<<count<<endl;
+    }
 };
 
 int employee :: count; //Default value is 0
@@ -25,15 +29,20 @@ int employee :: count; //Default value is 0
 int main()
 {
     employee piyush, rohan, mohan;
+    // count is the static data member of class employee
     // piyush.id = 1;
     // piyush.count = 1;   // cannot do this as id and count are private
     piyush.setData();
     piyush.getData();
+    employee :: getCount();
 
     rohan.setData();
     rohan.getData();
+    employee :: getCount();
 
     mohan.setData();
     mohan.getData();
+    employee :: getCount();
+
     return 0;
 }
