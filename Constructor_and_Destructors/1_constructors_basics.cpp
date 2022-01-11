8d05b4d6b335d9feaa1584aca6aa4414599fbaf4
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-11 15:38:30 +0530

    Add files via upload

---
+++ Constructor_and_Destructors/1_constructors_basics.cpp
@@ -0,0 +1,76 @@
+// Program to define a constructor and initialize the class data member variables with constants.
+
+// #include<iostream>
+
+// class num{
+//     private:
+//     int a,b,c;
+//     public:
+//     int x;
+//     num(void);      // Declaration of constructor
+//     void show(){
+//         std::cout<<"\nx = "<<x<<"\na = "<<a<<"\nb = "<<b<<"\nc = "<<c;
+//     }
+// };
+
+// num :: num(void){           // Definition of constructor
+//     std::cout<<"\nConstructor called";
+//     x = 5; a=0; b=1; c=2;
+// }
+
+// int main(){
+//     num n;
+//     n.show();
+//     return 0;
+// }
+
+
+
+
+// #include<iostream>
+
+// class num{
+//     private:
+//     int a;
+//     public:
+//     num(void){
+//         std::cout<<"\nConstructor called. ";
+//         a=1;
+//         std::cout<<"a = "<<a;
+//     }
+// };
+
+// int main(){
+//     num x,y,z;
+//     num a[2];
+//     return 0;
+// }
+
+
+
+
+// Program to read values from keyboard. Use constructor.
+
+// #include<iostream>
+
+// class num{
+//     int  a, b,c;
+//     public:
+//     num(void);           // declaration of constructor
+//     void show(){
+//         std::cout<<"\na = "<<a<<"\nb = "<<b<<"\nc = "<<c;
+//     }
+// };
+
+// num :: num(void){          // defu=inition of constructor
+//     std::cout<<"Constructor Called: ";
+//     std::cout<<"\nEnter values of a, b and c: ";
+//     std::cin>>a>>b>>c;
+// }
+
+// int main(){
+//     num n;
+//     n.show();
+//     return 0;
+// }
+
