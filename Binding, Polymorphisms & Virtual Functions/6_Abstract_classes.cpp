f041e29ceffe635bd6fbebed8ca3693e6370a33f
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-14 14:13:05 +0530

    Add files via upload

---
+++ Binding, Polymorphisms & Virtual Functions/6_Abstract_classes.cpp
@@ -0,0 +1,32 @@
+//                                ABSTRACT CLASSES
+
+// Program to use abstract class for program debugging.
+
+// #include<bits/stdc++.h>
+// // #include<abstract.h>
+
+// class A1 : public debug{
+//     int a;
+//     public:
+//     A1(int j = 0){
+//         a=j;
+//     }
+//     void show(){
+//         std::cout<<"\nIn class A1 a = "<<a;
+//     }
+// };
+
+// class A2 : public debug{
+//     int b;
+//     public:
+//     A2 (int k){
+//         b=k;
+//     }
+// };
+
+// int main(){
+//     A1 a(1);
+//     A2 b(5);
+//     a.show();
+//     b.show();
+// } 
