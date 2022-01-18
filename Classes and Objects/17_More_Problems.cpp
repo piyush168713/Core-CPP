3a4fbda7be630c6dfe900929b9d26cd125d08d73
Piyush Kumar <20BCS9107@cuchd.in>
2022-01-18 22:26:23 +0530

    Update 17_More_Problems.cpp

--- Classes and Objects/17_More_Problems.cpp
+++ Classes and Objects/17_More_Problems.cpp
@@ -653,3 +653,40 @@
 
 
 
+
+
+#include <bits/stdc++.h>
+  
+class Fibonacci {
+  
+public:
+    int a, b, c;
+    void generate(int);
+};
+  
+void Fibonacci::generate(int n)
+{
+    a = 0;
+    b = 1;
+  
+    std::cout << a << " " << b;
+  
+    for (int i = 1; i <= n - 2; i++) {
+  
+        c = a + b;
+        std::cout << " " << c;
+  
+        a = b;
+        b = c;
+    }
+}
+  
+int main()
+{
+    int n = 9;
+  
+    Fibonacci fib;
+    fib.generate(n);
+    return 0;
+}
+
